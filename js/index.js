9d0dc6da7ea8691c911b480e73f2a5b9c9af0e10
romilshah525 <romilshah525@gmail.com>
2020-05-12 19:59:35 +0530

    added courses and certifications

--- js/index.js
+++ js/index.js
@@ -1,39 +1,55 @@
-const Typewriter = function(txtElement, words, wait=400){
-	this.txtElement = txtElement;
-	this.words = words;
-	this.txt = '';
-	this.wordIndex = 0;
-	this.wait = parseInt(wait, 10);
-	this.type();
-	this.isDeleting = false;
+const Typewriter = function (txtElement, words, wait = 400) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = "";
+  this.wordIndex = 0;
+  this.wait = parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false;
+};
+Typewriter.prototype.type = function () {
+  const current = this.wordIndex % this.words.length;
+  const fullText = this.words[current];
+  if (this.isDeleting) {
+    this.txt = fullText.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullText.substring(0, this.txt.length + 1);
+  }
+  this.txtElement.innerHTML = `<span class="txt center-align">${this.txt}</span>`;
+  let typeSpeed = 250;
+  if (this.isDeleting) {
+    typeSpeed /= 2;
+  }
+  if (!this.isDeleting && this.txt === fullText) {
+    typeSpeed = this.wait;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.wordIndex++;
+    typeSpeed = 500;
+  }
+  setTimeout(() => this.type(), typeSpeed);
+};
+function init() {
+  const txtElement = document.querySelector(".txt-type");
+  const words = [
+    "Machine Learning",
+    "Web Development",
+    "Programming",
+    "Computer Engineering",
+  ];
+  const wait = 500;
+  new Typewriter(txtElement, words, wait);
 }
-Typewriter.prototype.type = function(){
-	const current = this.wordIndex % this.words.length;
-	const fullText = this.words[current];
-	if(this.isDeleting){
-		this.txt = fullText.substring(0, this.txt.length-1);
-	} else {
-		this.txt = fullText.substring(0, this.txt.length+1);
-	}
-	this.txtElement.innerHTML = `<span class="txt center-align">${this.txt}</span>`;
-	let typeSpeed = 250;
-	if(this.isDeleting) {
-		typeSpeed /= 2;
-	}
-	if(!this.isDeleting && this.txt === fullText){
-		typeSpeed = this.wait;
-		this.isDeleting = true;
-	} else if(this.isDeleting && this.txt === ''){
-		this.isDeleting = false;
-		this.wordIndex++;
-		typeSpeed = 500;
-	}
-	setTimeout(()=>this.type(), typeSpeed);
-}
-function init(){
-	const txtElement = document.querySelector('.txt-type');
-	const words = ["Machine Learning", "Web Development", "Programming", "Computer Engineering"];
-	const wait = 500;
-	new Typewriter(txtElement, words, wait);
-}
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
+
+$(document).ready(function () {
+  $(".tabs").tabs();
+  $(".scrollspy").scrollSpy();
+  $(".collapsible").collapsible();
+  $(".sidenav")
+    .sidenav()
+    .on("click tap", "li a", () => {
+      $(".sidenav").sidenav("close");
+    });
+});
